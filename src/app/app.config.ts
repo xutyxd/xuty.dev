1162269c731cde42bc3c565535e2df953a756707
Xuty <xutyxd@gmail.com>
2026-07-23 17:39:22 +0200

    feat(upgrade-ng): upgrade-ng to v22

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,12 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideClientHydration, withEventReplay, withNoIncrementalHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay(), withNoIncrementalHydration())
   ]
 };
